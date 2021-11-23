106ed2880ff546f1502426260cca604b0d4b81a3
Camilo U <cauribeva@unal.edu.co>
2021-11-22 20:50:07 -0500

    HU06#20 Added content to ProgramDTO Class

--- src/main/java/com/sofkau/academicsystembackend/models/program/ProgramDTO.java
+++ src/main/java/com/sofkau/academicsystembackend/models/program/ProgramDTO.java
@@ -1,4 +1,53 @@
 package com.sofkau.academicsystembackend.models.program;
 
+import java.util.Date;
+
 public class ProgramDTO {
+    private String id;
+    private String name;
+    private Date startingDate;
+    private List<CourseDTO> courses;
+
+    public ProgramDTO(String id, String name, Date startingDate, List<CourseDTO> courses) {
+        this.id = id;
+        this.name = name;
+        this.startingDate = startingDate;
+        this.courses = courses;
+    }
+
+    public ProgramDTO() {
+        super();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartingDate() {
+        return startingDate;
+    }
+
+    public void setStartingDate(Date startingDate) {
+        this.startingDate = startingDate;
+    }
+
+    public List<CourseDTO> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<CourseDTO> courses) {
+        this.courses = courses;
+    }
 }
