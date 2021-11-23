ca425b32bc4d5dff618af0ea94dc2415022a979d
Camilo U <cauribeva@unal.edu.co>
2021-11-23 13:47:15 -0500

    HU06#20 Added Program mapper utils

---
+++ src/main/java/com/sofkau/academicsystembackend/usecases/program/MapperUtilsProgram.java
@@ -0,0 +1,31 @@
+package com.sofkau.academicsystembackend.usecases.program;
+
+import com.sofkau.academicsystembackend.collections.program.Program;
+import com.sofkau.academicsystembackend.models.program.ProgramDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class MapperUtilsProgram {
+    public Function<ProgramDTO, Program> mapperToProgram(){
+        return programDTO -> {
+            var program = new Program();
+            program.setId(programDTO.getId());
+            program.setName(programDTO.getName());
+            program.setDuration(program.getDuration());
+            program.setCourses(programDTO.getCourses());
+            return program;
+        };
+    }
+
+    public Function<Program, ProgramDTO> mapperEntityToProgram(){
+        return program -> new ProgramDTO(
+                program.getId(),
+                program.getName(),
+                program.getDuration(),
+                program.getCourses()
+        );
+    }
+
+}
