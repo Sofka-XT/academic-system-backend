cf319200e183f3aae9abee08cacb2b5e4a6757e5
Camilo U <cauribeva@unal.edu.co>
2021-11-23 15:52:33 -0500

    HU06#12 Added create program router and usecase

--- src/main/java/com/sofkau/academicsystembackend/usecases/program/CreateProgramUseCase.java
+++ src/main/java/com/sofkau/academicsystembackend/usecases/program/CreateProgramUseCase.java
@@ -1,4 +1,26 @@
 package com.sofkau.academicsystembackend.usecases.program;
 
-public class CreateProgramUseCase {
+
+import com.sofkau.academicsystembackend.models.program.ProgramDTO;
+import com.sofkau.academicsystembackend.repositories.ProgramRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+@Service
+@Validated
+public class CreateProgramUseCase implements SaveProgram{
+    private final ProgramRepository programRepository;
+    private final MapperUtilsProgram mapperUtilsProgram;
+
+    public CreateProgramUseCase(ProgramRepository programRepository, MapperUtilsProgram mapperUtilsProgram) {
+        this.programRepository = programRepository;
+        this.mapperUtilsProgram = mapperUtilsProgram;
+    }
+
+    @Override
+    public Mono<ProgramDTO> apply(ProgramDTO programDTO) {
+        return programRepository.save(mapperUtilsProgram.mapperToProgram().apply(programDTO))
+                .map(course ->  mapperUtilsProgram.mapperEntityToProgram().apply(course));
+    }
 }
