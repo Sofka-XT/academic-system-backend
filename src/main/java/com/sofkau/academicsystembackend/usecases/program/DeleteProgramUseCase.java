94865e84ea73fdfe856c235ffe9eaaf353537569
Darley36 <darleybest@gmail.com>
2021-11-23 13:52:22 -0500

    HU06#15 Finished delete program

--- src/main/java/com/sofkau/academicsystembackend/usecases/program/DeleteProgramUseCase.java
+++ src/main/java/com/sofkau/academicsystembackend/usecases/program/DeleteProgramUseCase.java
@@ -1,4 +1,25 @@
 package com.sofkau.academicsystembackend.usecases.program;
 
-public class DeleteProgramUseCase {
+import com.sofkau.academicsystembackend.repositories.ProgramRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+@Validated
+public class DeleteProgramUseCase implements Function<String, Mono<Void>> {
+    private final ProgramRepository programRepository;
+
+    public DeleteProgramUseCase(ProgramRepository programRepository) {
+        this.programRepository = programRepository;
+    }
+
+    @Override
+    public Mono<Void> apply(String id) {
+        Objects.requireNonNull(id, "El id es requerido");
+        return programRepository.deleteById(id);
+    }
 }
