6bed6b7981f95dc44bbd5d511e39b67b8408cc38
Cgalvispadilla <fktcg99@gmail.com>
2021-11-22 20:44:09 -0500

    se creo el dto de la coleccion de los cursos referenciado a #5

--- src/main/java/com/sofkau/academicsystembackend/models/course/CourseDTO.java
+++ src/main/java/com/sofkau/academicsystembackend/models/course/CourseDTO.java
@@ -2,10 +2,41 @@ package com.sofkau.academicsystembackend.models.course;
 
 import com.sofkau.academicsystembackend.collections.course.Category;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import java.util.Set;
 
 public class CourseDTO {
 
+    private Set<Category> categories;
+    private String name;
+    private Integer id;
+
+    public CourseDTO(Integer id, String name, Set<Category> categories) {
+        this.id = id;
+        this.name = name;
+        this.categories = categories;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
