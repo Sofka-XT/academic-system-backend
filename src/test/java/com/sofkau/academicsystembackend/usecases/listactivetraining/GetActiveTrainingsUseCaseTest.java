61f98ab43ac0941b13ba30504f9294d155588d5b
Andres Cano <andrescaza246@hotmail.com>
2021-11-24 14:37:41 -0500

    HU02 #36 List active trainings UseCaseTest adapted to new changes

--- src/test/java/com/sofkau/academicsystembackend/usecases/listactivetraining/GetActiveTrainingsUseCaseTest.java
+++ src/test/java/com/sofkau/academicsystembackend/usecases/listactivetraining/GetActiveTrainingsUseCaseTest.java
@@ -1,9 +1,13 @@
 package com.sofkau.academicsystembackend.usecases.listactivetraining;
 
 
+import com.sofkau.academicsystembackend.collections.program.CourseTime;
 import com.sofkau.academicsystembackend.collections.program.Program;
+import com.sofkau.academicsystembackend.collections.program.Time;
 import com.sofkau.academicsystembackend.collections.training.Apprentice;
+import com.sofkau.academicsystembackend.collections.training.Coach;
 import com.sofkau.academicsystembackend.collections.training.Training;
+import com.sofkau.academicsystembackend.repositories.ProgramRepository;
 import com.sofkau.academicsystembackend.repositories.TrainingRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.util.*;
@@ -26,15 +31,65 @@ public class GetActiveTrainingsUseCaseTest {
     @MockBean
     TrainingRepository trainingRepository;
 
+    @MockBean
+    ProgramRepository programRepository;
+
     @Test
     void getAllActiveTrainingUseCaseTest() {
 
+        Time category1 = new Time();
+        category1.setCategoryId("1");
+        category1.setCategoryName("git");
+        category1.setDays(2);
+
+        Time category2 = new Time();
+        category2.setCategoryId("2");
+        category2.setCategoryName("java");
+        category2.setDays(4);
+
+        Time category3 = new Time();
+        category3.setCategoryId("3");
+        category3.setCategoryName("javascript");
+        category3.setDays(4);
+
+        Time category4 = new Time();
+        category4.setCategoryId("4");
+        category4.setCategoryName("Spring boot");
+        category4.setDays(5);
+
+        Time category5 = new Time();
+        category5.setCategoryId("5");
+        category5.setCategoryName("React");
+        category5.setDays(5);
+
+        Time category6 = new Time();
+        category6.setCategoryId("6");
+        category6.setCategoryName("Kotlin");
+        category6.setDays(3);
+
+        CourseTime courseTime1 = new CourseTime();
+        CourseTime courseTime2 = new CourseTime();
+        CourseTime courseTime3 = new CourseTime();
+
+        courseTime1.setCourseId("1");
+        courseTime1.setCourseName("Introduccion al desarrollo");
+        courseTime1.setCategories(new ArrayList<>(){{add(category1); add(category2); add(category3);}});
+
+        courseTime2.setCourseId("2");
+        courseTime2.setCourseName("Full stack");
+        courseTime2.setCategories(new ArrayList<>(){{add(category4); add(category5); add(category6);}});
+
+        courseTime3.setCourseId("3");
+        courseTime3.setCourseName("Introduccion al desarrollo Movil");
+        courseTime3.setCategories(new ArrayList<>(){{add(category6);}});
 
         // instancia del programa
-        Program program = new Program("1", "chao", new Date());
-        Program program2 = new Program("3", "See you later", new Date(80, 10, 11));
-        Program program3 = new Program("2", "sayonara", new Date(2021, 11, 19));
-
+        Program program = new Program("1", "chao",
+                new ArrayList<>(){{add(courseTime1);add(courseTime2);}});
+        Program program2 = new Program("2", "See you later",
+                new ArrayList<>(){{add(courseTime1);add(courseTime2); add(courseTime3);}});
+        Program program3 = new Program("3", "sayonara",
+                new ArrayList<>(){{add(courseTime1); add(courseTime3);}});
 
         // instancia del aprendiz 1
         Apprentice apprentice = new Apprentice();
@@ -51,57 +106,19 @@ public class GetActiveTrainingsUseCaseTest {
         ArrayList<Apprentice> apprenticeList3 = new ArrayList<Apprentice>();
         apprenticeList.add(apprentice);
 
-        var training = new Training("1", "train", program, LocalDate.now(), apprenticeList);
+        // instancia del coach 1
+        Coach coach = new Coach();
+        ArrayList<Coach> coachList = new ArrayList<>();
+        coachList.add(coach);
 
-        program.addCourse();
-        program.getCourses().get(0).setCategories(new HashMap<>());
-        program.getCourses().get(0).addTime("git", 2);
-        program.getCourses().get(0).addTime("java", 4);
-        program.getCourses().get(0).addTime("javascript", 4);
-        program.getCourses().get(0).SumTime();
-
-        program.addCourse();
-        program.getCourses().get(1).setCategories(new HashMap<>());
-        program.getCourses().get(1).addTime("Spring Boot", 5);
-        program.getCourses().get(1).addTime("React", 5);
-        program.getCourses().get(1).SumTime();
-
-        var training2 = new Training("2", "training", program2, LocalDate.now(), apprenticeList2);
-
-        program2.addCourse();
-        program2.getCourses().get(0).setCategories(new HashMap<>());
-        program2.getCourses().get(0).addTime("funcional", 2);
-        program2.getCourses().get(0).addTime("kotlin", 4);
-        program2.getCourses().get(0).addTime("go", 4);
-        program2.getCourses().get(0).SumTime();
-
-        program2.addCourse();
-        program2.getCourses().get(1).setCategories(new HashMap<>());
-        program2.getCourses().get(1).addTime("Ktor", 5);
-        program2.getCourses().get(1).addTime("Swift", 5);
-        program2.getCourses().get(1).SumTime();
-
-        training2.setProgram(program2);
-
-        var training3 = new Training("2", "training", program2, LocalDate.now(), apprenticeList2);
-
-        program3.addCourse();
-        program3.getCourses().get(0).setCategories(new HashMap<>());
-        program3.getCourses().get(0).addTime("reactive", 2);
-        program3.getCourses().get(0).addTime("skala", 4);
-        program3.getCourses().get(0).addTime("ruby", 4);
-        program3.getCourses().get(0).SumTime();
-
-        program3.addCourse();
-        program3.getCourses().get(1).setCategories(new HashMap<>());
-        program3.getCourses().get(1).addTime("laravel", 5);
-        program3.getCourses().get(1).addTime("node", 5);
-        program3.getCourses().get(1).SumTime();
-
-        training3.setProgram(program3);
-
+        var training = new Training("1", "train xxxx", program.getId(), LocalDate.now(), apprenticeList, coachList);
+        var training2 = new Training("2", "train yyyy", program2.getId(), LocalDate.of(2020,4, 27), apprenticeList2, coachList);
+        var training3 = new Training("3", "training zzzz", program3.getId(), LocalDate.of(2021,11, 18), apprenticeList3, coachList);
 
         Mockito.when(trainingRepository.findAll()).thenReturn(Flux.just(training, training2, training3));
+        Mockito.when(programRepository.findById("1")).thenReturn(Mono.just(program));
+        Mockito.when(programRepository.findById("2")).thenReturn(Mono.just(program2));
+        Mockito.when(programRepository.findById("3")).thenReturn(Mono.just(program3));
 
         var results = getActiveTrainingsUseCase.get();
 
