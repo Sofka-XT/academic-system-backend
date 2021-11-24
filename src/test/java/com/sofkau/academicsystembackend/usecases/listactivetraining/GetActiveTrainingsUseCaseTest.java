ad56dcbcc3b604280e9602b541a04ab3e4dbeebe
mvm11 <mvalenciaminota@gmail.com>
2021-11-24 15:51:40 -0500

    HU02 #36 Refactoring GetActiveTrainingsUseCaseTest

--- src/test/java/com/sofkau/academicsystembackend/usecases/listactivetraining/GetActiveTrainingsUseCaseTest.java
+++ src/test/java/com/sofkau/academicsystembackend/usecases/listactivetraining/GetActiveTrainingsUseCaseTest.java
@@ -34,8 +34,8 @@ public class GetActiveTrainingsUseCaseTest {
     @MockBean
     ProgramRepository programRepository;
 
-    @Test
-    void getAllActiveTrainingUseCaseTest() {
+
+    public List<Time> generateCategory() {
 
         Time category1 = new Time();
         category1.setCategoryId("1");
@@ -67,58 +67,143 @@ public class GetActiveTrainingsUseCaseTest {
         category6.setCategoryName("Kotlin");
         category6.setDays(3);
 
+        ArrayList<Time> categoriesList = new ArrayList<Time>();
+        categoriesList.add(category1);
+        categoriesList.add(category2);
+        categoriesList.add(category3);
+        categoriesList.add(category4);
+        categoriesList.add(category5);
+        categoriesList.add(category6);
+
+        return categoriesList;
+    }
+
+
+    public List<CourseTime> generateCourseTime() {
+        var categories = generateCategory();
+
         CourseTime courseTime1 = new CourseTime();
         CourseTime courseTime2 = new CourseTime();
         CourseTime courseTime3 = new CourseTime();
 
         courseTime1.setCourseId("1");
         courseTime1.setCourseName("Introduccion al desarrollo");
-        courseTime1.setCategories(new ArrayList<>(){{add(category1); add(category2); add(category3);}});
+        courseTime1.setCategories(new ArrayList<>() {{
+            add(categories.get(0));
+            add(categories.get(1));
+            add(categories.get(2));
+        }});
 
         courseTime2.setCourseId("2");
         courseTime2.setCourseName("Full stack");
-        courseTime2.setCategories(new ArrayList<>(){{add(category4); add(category5); add(category6);}});
+        courseTime2.setCategories(new ArrayList<>() {{
+            add(categories.get(3));
+            add(categories.get(4));
+            add(categories.get(5));
+        }});
 
         courseTime3.setCourseId("3");
         courseTime3.setCourseName("Introduccion al desarrollo Movil");
-        courseTime3.setCategories(new ArrayList<>(){{add(category6);}});
+        courseTime3.setCategories(new ArrayList<>() {{
+            add(categories.get(5));
+        }});
 
-        // instancia del programa
-        Program program = new Program("1", "chao",
-                new ArrayList<>(){{add(courseTime1);add(courseTime2);}});
+        ArrayList<CourseTime> courseTimeList = new ArrayList<CourseTime>();
+        courseTimeList.add(courseTime1);
+        courseTimeList.add(courseTime2);
+        courseTimeList.add(courseTime3);
+
+        return courseTimeList;
+    }
+
+    public List<Program> generateProgram() {
+        var courseTime = generateCourseTime();
+
+        Program program1 = new Program("1", "chao",
+                new ArrayList<>() {{
+                    add(courseTime.get(0));
+                    add(courseTime.get(1));
+                }});
         Program program2 = new Program("2", "See you later",
-                new ArrayList<>(){{add(courseTime1);add(courseTime2); add(courseTime3);}});
+                new ArrayList<>() {{
+                    add(courseTime.get(0));
+                    add(courseTime.get(1));
+                    add(courseTime.get(2));
+                }});
         Program program3 = new Program("3", "sayonara",
-                new ArrayList<>(){{add(courseTime1); add(courseTime3);}});
+                new ArrayList<>() {{
+                    add(courseTime.get(0));
+                    add(courseTime.get(1));
+                }});
 
-        // instancia del aprendiz 1
-        Apprentice apprentice = new Apprentice();
-        ArrayList<Apprentice> apprenticeList = new ArrayList<Apprentice>();
-        apprenticeList.add(apprentice);
+        ArrayList<Program> programList = new ArrayList<Program>();
+        programList.add(program1);
+        programList.add(program2);
+        programList.add(program3);
 
-        // instancia del aprendiz 2
+        return programList;
+    }
+
+    public List<Apprentice> generateApprentice() {
+
+        Apprentice apprentice1 = new Apprentice();
+        ArrayList<Apprentice> apprenticeList1 = new ArrayList<Apprentice>();
+        apprenticeList1.add(apprentice1);
+
         Apprentice apprentice2 = new Apprentice();
         ArrayList<Apprentice> apprenticeList2 = new ArrayList<Apprentice>();
-        apprenticeList.add(apprentice);
+        apprenticeList1.add(apprentice1);
 
-        // instancia del aprendiz 3
         Apprentice apprentice3 = new Apprentice();
         ArrayList<Apprentice> apprenticeList3 = new ArrayList<Apprentice>();
-        apprenticeList.add(apprentice);
+        apprenticeList1.add(apprentice1);
 
-        // instancia del coach 1
+        ArrayList<Apprentice> apprenticeList = new ArrayList<Apprentice>();
+        apprenticeList.add(apprentice1);
+        apprenticeList.add(apprentice2);
+        apprenticeList.add(apprentice3);
+
+        return apprenticeList;
+    }
+
+    public List<Coach> generateCoach() {
+
         Coach coach = new Coach();
         ArrayList<Coach> coachList = new ArrayList<>();
         coachList.add(coach);
 
-        var training = new Training("1", "train xxxx", program.getId(), LocalDate.now(), apprenticeList, coachList);
-        var training2 = new Training("2", "train yyyy", program2.getId(), LocalDate.of(2020,4, 27), apprenticeList2, coachList);
-        var training3 = new Training("3", "training zzzz", program3.getId(), LocalDate.of(2021,11, 18), apprenticeList3, coachList);
+        return coachList;
+    }
 
-        Mockito.when(trainingRepository.findAll()).thenReturn(Flux.just(training, training2, training3));
-        Mockito.when(programRepository.findById("1")).thenReturn(Mono.just(program));
-        Mockito.when(programRepository.findById("2")).thenReturn(Mono.just(program2));
-        Mockito.when(programRepository.findById("3")).thenReturn(Mono.just(program3));
+    public List<Training> generateTraining() {
+
+        var program = generateProgram();
+        var apprentice = generateApprentice();
+        var coach = generateCoach();
+
+        Training training = new Training("1", "train xxxx", program.get(0).getId(), LocalDate.now(), apprentice, coach);
+        Training training2 = new Training("2", "train yyyy", program.get(1).getId(), LocalDate.of(2020, 4, 27), apprentice, coach);
+        Training training3 = new Training("3", "training zzzz", program.get(2).getId(), LocalDate.of(2021, 11, 18), apprentice, coach);
+
+        ArrayList<Training> trainingList = new ArrayList<Training>();
+        trainingList.add(training);
+        trainingList.add(training2);
+        trainingList.add(training3);
+
+        return trainingList;
+    }
+
+
+    @Test
+    void getAllActiveTrainingUseCaseTest() {
+
+        var training = generateTraining();
+        var program = generateProgram();
+
+        Mockito.when(trainingRepository.findAll()).thenReturn(Flux.just(training.get(0), training.get(1), training.get(2)));
+        Mockito.when(programRepository.findById("1")).thenReturn(Mono.just(program.get(0)));
+        Mockito.when(programRepository.findById("2")).thenReturn(Mono.just(program.get(1)));
+        Mockito.when(programRepository.findById("3")).thenReturn(Mono.just(program.get(2)));
 
         var results = getActiveTrainingsUseCase.get();
 
